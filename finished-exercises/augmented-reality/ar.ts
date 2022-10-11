0fabbd982e949b7e717f6485068d541ccdf2a4c6
Wes Bos <wesbos@gmail.com>
2022-10-11 16:36:01 -0400

    update no-use-before-define

--- finished-exercises/augmented-reality/ar.ts
+++ finished-exercises/augmented-reality/ar.ts
@@ -1,21 +1,26 @@
+import { BarcodeDetectorPolyfill } from '@undecaf/barcode-detector-polyfill';
 import { codes } from './database';
 // import QrScanner from 'qr-scanner';
-import { BarcodeDetectorPolyfill } from '@undecaf/barcode-detector-polyfill'
+
+declare global {
+  interface Window {
+    BarcodeDetector: typeof BarcodeDetectorPolyfill;
+  }
+}
 
 try {
-  window['BarcodeDetector'].getSupportedFormats()
+  window.BarcodeDetector.getSupportedFormats();
 } catch {
-  window['BarcodeDetector'] = BarcodeDetectorPolyfill
+  window.BarcodeDetector = BarcodeDetectorPolyfill;
 }
 
 const app = document.querySelector<HTMLDivElement>('.app');
+if (!app) throw new Error('No app element found, aborting');
 const videoEl = app.querySelector<HTMLVideoElement>('video.webcam');
 const labelsEl = app.querySelector<HTMLDivElement>('.labels');
-const cameraSwitcherEl = document.querySelector<HTMLSelectElement>('select[name="camera"]');
-
-interface Window {
-  BarcodeDetector: any;
-}
+const cameraSwitcherEl = document.querySelector<HTMLSelectElement>(
+  'select[name="camera"]'
+);
 
 export interface BoundingBox {
   x: number;
@@ -43,13 +48,19 @@ export interface DetectedBarcode {
 const barcodeDetector = new window.BarcodeDetector({ formats: ['qr_code'] });
 
 async function populateVideo(cameraId?: string) {
+  const constraints: MediaStreamConstraints = {};
+  constraints.video = cameraId
+    ? {
+        deviceId: { exact: cameraId },
+        height: { min: 576, ideal: 720, max: 1080 },
+      }
+    : true;
 
-  const constraints: MediaStreamConstraints = { };
-  constraints.video = cameraId ? { deviceId: { exact: cameraId },         height: { min: 576, ideal: 720, max: 1080 }, } : true;
+  const stream = await navigator.mediaDevices
+    .getUserMedia(constraints)
+    .catch(console.error);
 
-  const stream = await navigator.mediaDevices.getUserMedia(constraints).catch(console.error);
-
-  if(!stream) return;
+  if (!stream || !videoEl) return;
   videoEl.srcObject = stream;
   videoEl.muted = true;
   await videoEl.play();
@@ -59,9 +70,8 @@ async function populateVideo(cameraId?: string) {
 }
 
 async function detectBarcodes() {
-
   const results: DetectedBarcode[] = await barcodeDetector.detect(videoEl);
-  results.forEach(result => {
+  results.forEach((result) => {
     // console.log('Detected barcode', result.rawValue);
     // console.table(result.cornerPoints);
     // console.table(result.boundingBox);
@@ -76,16 +86,18 @@ async function detectBarcodes() {
     // console.table(result.cornerPoints);
     const [topLeft, topRight, bottomRight, bottomLeft] = result.cornerPoints;
     // select the label for this element
-    const label = labelsEl.querySelector<HTMLSpanElement>(`[data-code="${result.rawValue}"]`);
+    const label = labelsEl.querySelector<HTMLSpanElement>(
+      `[data-code="${result.rawValue}"]`
+    );
     // label.style.width = `${width}px`;
     // label.style.height = `${height}px`;
     label.style.left = `${scaledCenterPoint.x}px`;
     label.style.top = `${scaledCenterPoint.y}px`;
-  })
+  });
 }
 
 async function createLabels() {
-  Object.values(codes).forEach(code => {
+  Object.values(codes).forEach((code) => {
     const el = document.createElement('span');
     el.classList.add('label');
     el.innerText = code.name;
@@ -96,14 +108,14 @@ async function createLabels() {
 
 async function populateCameras() {
   // if there is a list of cameras already, don't bother
-  if(cameraSwitcherEl.length) return;
+  if (cameraSwitcherEl.length) return;
   // get all devices
   const devices = await navigator.mediaDevices.enumerateDevices();
   console.table(devices);
   // loopm over all video imputs and create an option for them
   devices
-    .filter(d => d.kind === 'videoinput')
-    .forEach(device => {
+    .filter((d) => d.kind === 'videoinput')
+    .forEach((device) => {
       const option = document.createElement('option');
       option.value = device.deviceId;
       option.innerText = device.label;
@@ -112,19 +124,19 @@ async function populateCameras() {
 }
 
 function handleCameraChange() {
-  populateVideo(cameraSwitcherEl.value)
+  populateVideo(cameraSwitcherEl.value);
 }
 
 function scaleCoordsToVideo(x: number, y: number) {
   const videoRect = videoEl.getBoundingClientRect();
   // videoRect.width / videoEl.videoWidth givies us a multiplier to scale the x and y coords. Say we have 1920 video, but it's being displayed at 500px wide. Then we will have 500 / 1920 = 0.2604166667. A point that used to be at 500, is now 500 * 0.2604166667.
   return {
-    x: x * videoRect.width / videoEl.videoWidth,
-    y: y * videoRect.height / videoEl.videoHeight,
-  }
+    x: (x * videoRect.width) / videoEl.videoWidth,
+    y: (y * videoRect.height) / videoEl.videoHeight,
+  };
 }
 
-cameraSwitcherEl.addEventListener('change', handleCameraChange)
+cameraSwitcherEl.addEventListener('change', handleCameraChange);
 
 populateVideo();
 createLabels();
