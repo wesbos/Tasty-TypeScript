8bcbf7ab6af2b65f57bcc6bbbd4c192c163e7c12
Wes Bos <wesbos@gmail.com>
2022-10-11 13:05:16 -0400

    fix broken main links

--- finished-exercises/augmented-reality/vite.config.ts
+++ finished-exercises/augmented-reality/vite.config.ts
@@ -1,13 +1,10 @@
 import { defineConfig } from 'vite';
-import { esbuildCommonjs } from '@originjs/vite-plugin-commonjs'
-
+import { esbuildCommonjs } from '@originjs/vite-plugin-commonjs';
 
 export default defineConfig({
   optimizeDeps: {
     esbuildOptions: {
-      plugins: [
-        esbuildCommonjs(['zbar.wasm'])
-      ]
-    }
-  }
+      plugins: [esbuildCommonjs(['zbar.wasm'])],
+    },
+  },
 });
