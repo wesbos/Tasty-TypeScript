b92223ed70c505d8ccada135bbb40fd64cc50f54
Wes Bos <wesbos@gmail.com>
2022-10-11 12:10:01 -0400

    vite config

--- exercises/test-exercise-1/test.js
+++ exercises/test-exercise-1/test.js
@@ -1,1 +1,2 @@
 const age = 10;
+hey;
--- exercises/with-external-deps/cool.ts
+++ exercises/with-external-deps/cool.ts
@@ -1,3 +1,5 @@
 import convert from 'color-convert';
+
+const age = 100;
 // this should throw an error about string not being type of RGB
 convert.rgb.hex('hello');
