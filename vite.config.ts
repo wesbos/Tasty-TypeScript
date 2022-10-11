52da5e6d5976d7935077004d42b8fc1f5d95ddde
Wes Bos <wesbos@gmail.com>
2022-10-11 13:00:10 -0400

    oops

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 /* eslint-disable */
 import { defineConfig, PluginOption } from 'vite';
 // import { directoryPlugin } from 'vite-plugin-list-directory-contents';
-import { directoryPlugin } from '../../../Sites/vite-plugin-list-directory-contents/plugin';
+import { directoryPlugin } from 'vite-plugin-list-directory-contents';
 
 export default defineConfig({
   // A simple vite config. We are using 1 plugin here so we can visually navigate the directory structure. Funny things here - Vite is whining about the plugin not having the correct params, because their types are missing something. So here we cast it with `as`.
